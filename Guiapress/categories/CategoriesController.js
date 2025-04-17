2f6d214fa642cbe51dddbe132ff4c14229fdf845
arthurMalveste <arthurmielemalveste@gmail.com>
2025-04-17 15:37:10 -0300

    comeco da edicao

--- Guiapress/categories/CategoriesController.js
+++ Guiapress/categories/CategoriesController.js
@@ -11,7 +11,7 @@ router.post('/categories/save', (req, res) => {
             slug: slugify(title)
             
         }).then(() => {
-            res.redirect('/');
+            res.redirect('/admin/categories');
         })
     }else {
         res.redirect('/admin/categories/new');
@@ -26,6 +26,40 @@ router.get('/admin/categories', (req, res) => {
 
     Category.findAll().then(categories => {
         res.render("admin/categories/index.ejs", { categories: categories });
+    });
+});
+
+router.post("/categories/delete", (req, res) => { 
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){ //for um número
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories');
+            });
+        }else{
+            res.redirect('/admin/categories');
+        }
+    }
+
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect('/admin/categories');
+    }
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+            res.render('admin/categories/edit', { category: category });
+        }else{
+            res.redirect('/admin/categories');
+        }
+    }).catch(erro => {
+        res.redirect('/admin/categories');
     });
 });
 
